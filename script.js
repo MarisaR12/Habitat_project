3a0e33169e8602b57159e688cb52d97468f31ec7
M.Ramautar <144722341+MarisaR12@users.noreply.github.com>
2025-12-22 17:34:53 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+$(document).ready(function(){
+  $(".btn1").click(function(){
+    $(".content").slideDown();
+  });
+});
 
+$(document).ready(function(){
+  $(".btn2").click(function(){
+    $(".content").slideUp();
+  });
+});
+
+$(document).ready(function(){
+  $(".btn3").click(function(){
+    $(".build_content").css('color','black');
+    $(".building").show();
+
+  });
+});
+
+$(document).ready(function(){
+  $(".btn4").click(function(){
+    $(".audio").fadeIn();
+  });
+});
